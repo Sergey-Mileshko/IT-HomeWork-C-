7231620b887746ad067bc68816fe5a05590388ee
Sergey-Mileshko <84710491+Sergey-Mileshko@users.noreply.github.com>
2024-04-21 00:17:32 +0300

    Add files via upload

---
+++ HW1_Mileshko/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Забивание гвоздя HW1_Mileshko");
+
+        
+                Console.WriteLine("Возьмите гвоздь от 1 до 10");
+
+        int N = System.Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Берём молоток");
+        Console.WriteLine("Вы ввели такое кол-во гвоздей "+N);
+     
+        Console.WriteLine("Делаем удар молотком по гвоздю, от 1 до 5 - кол-во ударов ");
+
+        int T = System.Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Кол-во ударов " + T);
+   
+
+        if (N < 0)
+        
+        for (int i = 1; N < i; N--)
+            {
+                Console.WriteLine("Возьмите следующий гвоздь");
+        }
+        else
+        if (T==0)
+        {
+            Console.WriteLine("Гвоздь забит");
+        }
+
+    }
+}
