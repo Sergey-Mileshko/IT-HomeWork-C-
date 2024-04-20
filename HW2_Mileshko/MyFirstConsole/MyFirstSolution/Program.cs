cbe95d8a3636f6eb140294940acdb0b9a72bcbd4
Sergey-Mileshko <84710491+Sergey-Mileshko@users.noreply.github.com>
2024-04-21 00:22:31 +0300

    Add files via upload

---
+++ HW2_Mileshko/MyFirstConsole/MyFirstSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задание 1 HW2
+        Console.WriteLine("MyFirstConsole_Mileshko ");
+        int i = 10;
+        Console.WriteLine("Заданно число i= "+i);
+        Console.ReadKey();
+
+    }
+}
---
+++ HW2_Mileshko/MySecondConsole/MyFirstSolution/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+internal class Program
+{
+    public const double Pi = Math.PI;
+    
+    private static void Main(string[] args)
+    {
+
+       
+        Console.WriteLine("MySecondConsole_Mileshko\n");
+     
+        // задание2 HW2
+        Console.WriteLine("Задание2 HW2 ");
+       // double pi = 3.141592653d;
+        Console.WriteLine(" Введено число Pi= " + Pi);
+        
+    decimal e = 1.414213562373095048M;
+
+        Console.WriteLine(" Введено число e=" + e) ;
+
+        double Ed = 1.414213562373095048d;
+        Console.WriteLine("Ed=" + Ed); Console.Write("Ed- тип double" );
+        // задание3 HW2
+        Console.WriteLine("\nЗадание3 HW2 ");
+        int a = 3;
+        Console.WriteLine(" a= "+ a);
+
+        byte b = ((byte)a);
+        Console.WriteLine(" b= "+ b);
+
+
+        // задание4 HW2
+        Console.WriteLine("\nЗадание4 HW2 ");
+        bool correctEnter = false;
+        int x = 0, y = 0, z = 0;
+        string input = "";
+        do
+        {
+            Console.WriteLine("Введите 1-e  число");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out int result1))
+            {
+
+                correctEnter = true;
+                x = result1;
+            }
+            else Console.WriteLine("Не корректный ввод");
+
+            correctEnter = false;
+
+            Console.WriteLine("Введите 2-e  число");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out int result2))
+            {
+
+                correctEnter = true;
+                y = result2;
+            }
+            else Console.WriteLine("Не корректный ввод");
+
+            correctEnter = false;
+
+            Console.WriteLine("Введите 3-e  число");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out int result3))
+            {
+
+                correctEnter = true;
+                z = result3;
+            }
+            else Console.WriteLine("Не корректный ввод");
+
+            correctEnter = false;
+
+            double rez = (x + y + z) / 3;
+
+            Console.WriteLine("rez= " + rez);
+
+
+        }
+        while (true);
+
+        Console.ReadKey();
+    }
+}
