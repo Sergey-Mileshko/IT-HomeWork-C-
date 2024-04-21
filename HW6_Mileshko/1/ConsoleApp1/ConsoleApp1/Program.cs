7a7528633540be1d4f8f3d8ff603a3f96116a24c
Sergey-Mileshko <84710491+Sergey-Mileshko@users.noreply.github.com>
2024-04-21 17:25:40 +0300

    Add files via upload

---
+++ HW6_Mileshko/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿internal class Program
+{
+   
+        public static string[] AddValueToArray(string[] array, string value)
+    {
+        bool b_Add = false;
+        if (array == null || string.IsNullOrEmpty(value))
+        {
+            return array;
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] != null)
+            {
+                if (array[i].ToUpper() == value.ToUpper())
+                {
+                    return array;
+                }
+            }
+
+        }
+
+        foreach (string S in array)
+        {
+            if (S != null)
+            {
+                if (S.ToUpper() == value.ToUpper())
+                {
+                    return array;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (string.IsNullOrEmpty(array[i]))
+            {
+                array[i] = value;
+                b_Add = true;
+                return array;
+            }
+        }
+
+
+        if (!b_Add)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = value;
+        }
+        return array;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HW6_ex1_Mileshko");
+        Console.WriteLine("Words:");
+        string[] array = { "banana", "orange", "egg", "pasta", "pineapple" };
+
+        array = AddValueToArray(array, "");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+        Console.WriteLine("Number of words:"); ;
+        Console.WriteLine(array.Length);
+        Console.ReadLine();
+
+        Console.ReadKey();
+
+    }
+
+
+}
+
+
---
+++ HW6_Mileshko/2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+   
+        public static int[,] SumArray(int[,] a, int[,] b)
+        {
+            int[,] result = new int[a.GetLength(0), a.GetLength(1)];
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return result;
+        }
+        private static void Main(string[] args)
+        {
+        Console.WriteLine("HW6_ex2_Mileshko");
+
+            int[,] a = new int[2, 2] { { 1, 4 }, { 2, 3 } };
+        
+            int[,] b = new int[2, 2] { { 4, 4 }, { 5, 2 } };
+
+
+            int[,] sum = SumArray(a, b);
+        Console.WriteLine("Result A+B:");
+
+        for (int i = 0; i < sum.GetLength(0); i++)
+            {
+                for (int j = 0; j < sum.GetLength(1); j++)
+                {
+                    Console.Write(sum[i, j] + " ");
+                }
+                Console.WriteLine();
+          
+            }
+        }
+    }
