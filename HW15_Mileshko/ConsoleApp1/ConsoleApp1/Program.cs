9f71f41a69f69730c409b42760900b080392513f
Sergey-Mileshko <84710491+Sergey-Mileshko@users.noreply.github.com>
2024-04-22 19:50:06 +0300

    Add files via upload

---
+++ HW15_Mileshko/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class School
+{
+    public delegate void StudentAddedDelegate(string name, int age);
+    public event StudentAddedDelegate StudentAdded;
+
+    public void AddStudent(string name, int age)
+    {
+        Console.WriteLine($"People {name} add to school.");
+        StudentAdded?.Invoke(name, age);
+    }
+}
+
+class Director
+{
+    public void GreetParents(string name, int age)
+    {
+        Console.WriteLine($"Welcome to school, parents of {name}! You child is {age} old. \n");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HW15_ex1_Mileshko \n");
+
+        School school = new School();
+        Director director = new Director();
+        school.StudentAdded += director.GreetParents;
+        school.AddStudent("Ivan", 15);
+        school.AddStudent("Lev", 7);
+        school.AddStudent("Leya", 9);
+        school.AddStudent("Sofia", 10);
+
+        Console.ReadKey();
+    }
+}
---
+++ HW15_Mileshko/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Kindergarten
+{
+    public event EventHandler ChildGraduated;
+    public void GraduateChild(string childName)
+    {
+        Console.WriteLine($"Child is {childName} graduated from kindergarten.");
+        ChildGraduated?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class School
+{
+    public void AcceptChild(object sender, EventArgs e)
+    {
+        Console.WriteLine("The child has add to school. \n");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HW15_ex2_Mileshko \n");
+        Kindergarten kindergarten = new Kindergarten();
+        School school = new School();
+        kindergarten.ChildGraduated += school.AcceptChild;
+
+        kindergarten.GraduateChild("Yasha");
+        kindergarten.GraduateChild("Dasha");
+        kindergarten.GraduateChild("Ivan");
+
+        Console.ReadKey();
+    }
+}
