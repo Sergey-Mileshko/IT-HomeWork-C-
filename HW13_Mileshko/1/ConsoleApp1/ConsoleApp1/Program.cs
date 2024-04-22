4c79816e4e3972ed1f532b5fd4dbad1a25039211
Sergey-Mileshko <84710491+Sergey-Mileshko@users.noreply.github.com>
2024-04-22 18:12:31 +0300

    Add files via upload

---
+++ HW13_Mileshko/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static IEnumerable<string> GetWordsGreaterThanFive(string[] words)
+    {
+        foreach (var word in words)
+        {
+            if (word.Length > 5)
+            {
+                yield return word;
+            }
+        }
+    }
+
+    static string[] GenerateRandomWords(int count)
+    {
+        Dictionary<int, string> wordDictionary = new Dictionary<int, string>()
+        {
+            { 1, "School backpack" },
+            { 2, "Pencil" },
+            { 3, "Ruler" },
+            { 4, "Textbook" },
+            { 5, "Mobile phone" },
+            { 6, "Money" },
+            { 7, "Ball" },
+            { 8, "Hat" },
+            { 9, "Glasses" },
+            { 10, "Sun glasses" }
+        };
+
+        Random random = new Random();
+        List<string> randomWords = new List<string>();
+
+        while (randomWords.Count < count)
+        {
+            int randomKey = random.Next(1, wordDictionary.Count + 1);
+            string randomWord = wordDictionary[randomKey];
+
+            if (!randomWords.Contains(randomWord))
+            {
+                randomWords.Add(randomWord);
+            }
+        }
+
+        return randomWords.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("HW13_ex1_Mileshko \n");
+        string[] randomWords = GenerateRandomWords(3);
+
+        Console.WriteLine("Words randomWords:");
+
+        foreach (string s in randomWords) Console.Write(s + " ");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("\nWords longer than 5 characters:");
+
+        foreach (var word in GetWordsGreaterThanFive(randomWords))
+        {
+            Console.WriteLine(word);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ HW13_Mileshko/2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Collection<T> where T : IComparable<T>
+{
+    private List<T> items = new List<T>();
+
+    public void Add(T item)
+    {
+        items.Add(item);
+        items.Sort();
+    }
+
+    public void Display()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine(item.ToString());
+        }
+
+        Console.WriteLine();
+    }
+}
+
+class MyClass : IComparable<MyClass>
+{
+    public int IdNumber { get; set; }
+
+    public int CompareTo(MyClass other)
+    {
+        return IdNumber.CompareTo(other.IdNumber);
+    }
+
+    public override string ToString()
+    {
+        return $"IdNumber: {IdNumber}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HW13_ex2_Mileshko \n");
+        Collection<MyClass> collection = new Collection<MyClass>();
+        Console.WriteLine("Sort the list: ");
+        collection.Add(new MyClass { IdNumber = 3 });
+        collection.Add(new MyClass { IdNumber = 5 });
+        collection.Add(new MyClass { IdNumber = 2 });
+        collection.Add(new MyClass { IdNumber = 6 });
+        collection.Add(new MyClass { IdNumber = 4 });
+        collection.Add(new MyClass { IdNumber = 1 });
+
+        collection.Display();
+        Console.WriteLine("Add number in sort the list: ");
+        collection.Add(new MyClass { IdNumber = 7 });
+
+        collection.Display();
+        Console.ReadKey();
+    }
+}
